4364be7efcc16b8b54e6b397358795c039d7728c
mathewthomasfiles <125761780+mathewthomasfiles@users.noreply.github.com>
2026-03-20 05:55:05 +0530

    Add files via upload

---
+++ 03/index.mm
@@ -0,0 +1,131 @@
+<map version="freeplane 1.12.15">
+<!--To view this file, download free mind mapping software Freeplane from https://www.freeplane.org -->
+<bookmarks/>
+<node TEXT="HC Case Listing Compilation" STYLE_REF="Normal" FOLDED="false" ID="ID_1073424294" CREATED="1568034565282" MODIFIED="1773963318450" NUMBERED="false"><hook NAME="MapStyle" zoom="1.2">
+    <properties show_icon_for_attributes="true" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" show_tags="UNDER_NODES" show_note_icons="true" fit_to_viewport="false" show_icons="BESIDE_NODES" showTagCategories="false"/>
+    <tags category_separator="::"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="bottom_or_right" ID="ID_1676003596" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ID="ID_1328378886" ICON_SIZE="12 pt" COLOR="#0000ff" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<arrowlink SHAPE="CUBIC_CURVE" COLOR="#000000" WIDTH="2" TRANSPARENCY="200" DASH="" FONT_SIZE="9" FONT_FAMILY="SansSerif" DESTINATION="ID_1328378886" STARTARROW="NONE" ENDARROW="DEFAULT"/>
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.tags">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.selection" BACKGROUND_COLOR="#4e85f8" BORDER_COLOR_LIKE_EDGE="false" BORDER_COLOR="#4e85f8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="bottom_or_right" ID="ID_90203482" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" ID="ID_96560120" COLOR="#18898b" STYLE="fork" NUMBERED="true">
+<font NAME="Arial" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" ID="ID_1653091571" COLOR="#cc3300" STYLE="fork" NUMBERED="true">
+<font NAME="Arial" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" ID="ID_707370257" FORMAT_AS_HYPERLINK="true" COLOR="#669900" NUMBERED="true">
+<font NAME="Arial" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important" ID="ID_1591728848" NUMBERED="true">
+<icon BUILTIN="yes"/>
+<font NAME="Arial" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Normal" ID="ID_1936742016" COLOR="#0000ff" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Answered" ID="ID_1976895800" COLOR="#006600" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<icon BUILTIN="button_ok"/>
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Forwarded" ID="ID_717575758" COLOR="#ff00ff" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<icon BUILTIN="smiley-oh"/>
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Unsatisfactory" ID="ID_1584537951" COLOR="#ff6600" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<icon BUILTIN="smily_bad"/>
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Delayed" ID="ID_620294510" COLOR="#cc0000" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<icon BUILTIN="calendar"/>
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="No Response" ID="ID_123366198" COLOR="#cc0000" BACKGROUND_COLOR="#ffff33" STYLE="bubble" NUMBERED="true">
+<icon BUILTIN="messagebox_warning"/>
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="Audit" ID="ID_754984979" COLOR="#0000ff" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<icon BUILTIN="xmag"/>
+<font NAME="Arial" SIZE="10" BOLD="false" ITALIC="true"/>
+</stylenode>
+<stylenode TEXT="TBD - Overdue" ID="ID_1259270799" COLOR="#ff0000" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="TBD - In Progress" ID="ID_181776389" COLOR="#ff00ff" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<font NAME="Arial" SIZE="10" BOLD="false"/>
+</stylenode>
+<stylenode TEXT="TBD - Done" ID="ID_730955583" COLOR="#009900" BACKGROUND_COLOR="#ffffff" STYLE="fork" NUMBERED="true">
+<icon BUILTIN="button_ok"/>
+<font NAME="Arial" SIZE="10" BOLD="false" STRIKETHROUGH="true"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="bottom_or_right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10 pt" SHAPE_VERTICAL_MARGIN="10 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="56" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AlwaysUnfoldedNode"/>
+<node TEXT="Freemind Html" POSITION="bottom_or_right" ID="ID_451177620" CREATED="1773963261963" MODIFIED="1773963406644" LINK="FreeMindHtml.html">
+<edge COLOR="#00ff00"/>
+</node>
+<node TEXT="Freemind XHTML" POSITION="bottom_or_right" ID="ID_1061326372" CREATED="1773963431225" MODIFIED="1773963464987" LINK="FreePlaneXhtml.html">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Xmind01 HTML" POSITION="bottom_or_right" ID="ID_1686125511" CREATED="1773963466820" MODIFIED="1773963512036" LINK="XMind01Html.html">
+<edge COLOR="#007c00"/>
+</node>
+<node TEXT="Xmind02 PDF To HTML" POSITION="bottom_or_right" ID="ID_1845277448" CREATED="1773966016418" MODIFIED="1773966046992" LINK="XMind02Pdf.html">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="XMind01 pdf" POSITION="bottom_or_right" ID="ID_1457290289" CREATED="1773963515208" MODIFIED="1773963555843" LINK="XMind01Pdf.pdf">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="Xmind02 PDF" POSITION="bottom_or_right" ID="ID_1269581484" CREATED="1773963557836" MODIFIED="1773963581522" LINK="XMind02Pdf.pdf">
+<edge COLOR="#007c7c"/>
+</node>
+<node TEXT="Xmind02 PDF Outliner" POSITION="bottom_or_right" ID="ID_304547956" CREATED="1773963582979" MODIFIED="1773963609220" LINK="XMind02PdfOutliner.pdf">
+<edge COLOR="#7c7c00"/>
+</node>
+</node>
+</map>
